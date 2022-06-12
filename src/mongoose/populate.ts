eacddae6df48572f5026600d6675d4a4d3712955
Pablo Ibanez <me@pabloibanez.com>
2022-06-13 00:00:14 +0100

    feat(#56): business new props

--- src/mongoose/populate.ts
+++ src/mongoose/populate.ts
@@ -1,3 +1,6 @@
 export const basicPerson = 'firstName lastName email gender color picture';
 
 export const basicCompany = [{ path: 'name' }, { path: 'director', select: basicPerson }];
+
+export const basicTaxYear = 'name country startDate endDate';
+export const fullTaxYear = 'name country startDate endDate incomeTax dividendsTax vatBands';
