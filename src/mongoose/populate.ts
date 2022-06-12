29db8f7ea7594afaf5dddc4d7ef7379b1be9ecac
Pablo Ibanez <me@pabloibanez.com>
2022-06-12 18:07:34 +0100

    feat(#50): getuser payload contains person data

--- src/mongoose/populate.ts
+++ src/mongoose/populate.ts
@@ -1,3 +1,3 @@
-export const basicPerson = 'firstName lastName email color';
+export const basicPerson = 'firstName lastName email gender color picture';
 
 export const basicCompany = [{ path: 'name' }, { path: 'director', select: basicPerson }];
